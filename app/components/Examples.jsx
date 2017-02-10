831b8da3bd5b2457c892e347bd51f8b4a23a65ab
Moshe Itzhaki <moshe.z.itzhaki@gmail.com>
2017-02-10 17:38:46 +0200

    Add grid system & style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {                  //stateless components
   return (
     <div>
-    <h3>Examples</h3>
-    <p>Welcome to examples page!</p>
-    </div>
+    <h1 ClassName="text-center">Examples</h1>
+    <p>Here are a few examples location to try out:</p>
+    <ol>
+      <li>
+        <Link to='/?location=New York City'>New-York City, NYC</Link>
+      </li>
+      <li>
+        <Link to='/?location=Rio'>Rio, Brazil</Link>
+      </li>
+    </ol>
+  </div>
   )
 };
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,11 @@ var Main = (props) => {             //stateless components
   return (
     <div>
       <Nav/>
-      <h2>Main component</h2>
-      {props.children}
+      <div ClassName="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 }
